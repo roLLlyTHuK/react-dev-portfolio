dd22dc9a8124b492a1a47b75b15f212fa176566f
roLLlyTHuK <ircvipua@gmail.com>
2024-01-18 12:52:09 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Test from "./Test";
 import "./app.scss";
+import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
@@ -29,9 +28,6 @@ const App = () => {
       <section id="Contact">
         <Contact />
       </section>
-      {/* Framer Motion Crash Course */}
-      {/* <Test/>
-    <Test/> */}
     </div>
   );
 };
