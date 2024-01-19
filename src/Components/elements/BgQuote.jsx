6a09650168e6fe1d5093f825e6c8f7f26bc2a997
roLLlyTHuK <ircvipua@gmail.com>
2024-01-19 13:02:12 +0200

    up

--- src/Components/elements/BgQuote.jsx
+++ src/Components/elements/BgQuote.jsx
@@ -1,7 +1,7 @@
-import { m, LazyMotion, domAnimation } from "framer-motion";
-import Arrow from "./Arrow";
+import { m, LazyMotion, domAnimation } from 'framer-motion';
+import Arrow from './Arrow';
 
-const BgQuote = (props) => {
+const BgQuote = props => {
   const { text } = props;
 
   return (
@@ -9,11 +9,11 @@ const BgQuote = (props) => {
       <m.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.6, type: "spring", delay: 0.3 }}
+        transition={{ duration: 0.6, type: 'spring', delay: 0.3 }}
         className="absolute top-[10%] sm:top-[10%] md:top-[10%] left-[-60%] sm:left-[-30%] md:left-[-15%] lg:left-[-5%] xl:left-[10%] w-fit h-fit flex flex-col rotate-[-45deg]"
       >
         <p
-          style={{ fontFamily: "La Belle Aurore, cursive" }}
+          style={{ fontFamily: 'La Belle Aurore, cursive' }}
           className="text-grayscale-600 text-lg noselect"
         >
           {text}
