14b8195ae1f5a0e2cc68eca65eee72acecef7c70
roLLlyTHuK <ircvipua@gmail.com>
2024-01-20 23:37:17 +0200

    up

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import PreloadMedia from "../Components/PreloadMedia";
 import { media } from "../Constants/constants";
-import "font-awesome/css/font-awesome.min.css";
-// import "./Fonts/Morganite/morganiteFont.css";
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="">
       <PreloadMedia images={Object.values(media)}>
         <App />
       </PreloadMedia>
