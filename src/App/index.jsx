9e1818019683ce007c0ba2becd6ee20731f8c9f5
roLLlyTHuK <ircvipua@gmail.com>
2024-01-20 21:05:25 +0200

    up

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import PreloadMedia from "./Components/PreloadMedia";
-import { media } from "./Constants/constants";
+import PreloadMedia from "../Components/PreloadMedia";
+import { media } from "../Constants/constants";
 import "font-awesome/css/font-awesome.min.css";
 // import "./Fonts/Morganite/morganiteFont.css";
-import App from '/App';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
